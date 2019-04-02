a16efeeb456f952e67f84f089fac25b49f676657
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-04-02 12:56:36 +0800

    Added jquery nav and google analyitics

---
+++ resources/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction === "down") {
+            $('nav').addClass('sticky');
+
+        } else {
+            $('nav').removeClass('sticky');
+
+        }
+    }, {
+        offset: '70px'
+    });
+  
+
+});
